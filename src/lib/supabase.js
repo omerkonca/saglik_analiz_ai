bffc4fb5f285bcca67e3245fb86a0c5872012283
Ömer Faruk KONCA <190541018@firat.edu.tr>
2024-12-25 14:00:19 +0300

    update

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -5,3 +5,4 @@ var supabase_js_1 = require("@supabase/supabase-js");
 var supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 var supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
+
